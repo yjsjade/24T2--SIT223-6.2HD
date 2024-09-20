d1d6f03c220dfdb15757bf5bfdf8f26f68ec863a
jade <jingshii.y@gmail.com>
2024-09-20 13:11:21 +1000

    app

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Timers;
-using Microsoft.VisualBasic;
 using Timer = System.Timers.Timer;
 
 
@@ -27,51 +26,6 @@ namespace _54HD
 
         static void Main(string[] args)
         {
-            bool isInteractive = !Console.IsInputRedirected;
-
-            if (isInteractive)
-            {
-                DisplayMenu();
-            } else
-            {
-                Console.WriteLine("Running in non - interactive mode.");
-            }
-
-
-            // Create a time for Tick event
-            Timer timer = new Timer(10);
-            // Hook up the Elapsed event for the timer. 
-            timer.Elapsed += OnTimedEvent;
-            timer.AutoReset = true;
-
-            // Connect GUI with the Controller and vice versa
-            contoller = new EnhancedReactionController();
-            gui = new Gui();
-            gui.Connect(contoller);
-            contoller.Connect(gui, new RandomGenerator());
-
-            //Reset the GUI
-            gui.Init();
-            // Start the timer
-            timer.Enabled = true;
-
-            if (isInteractive)
-            {
-                RunInteractiveMenu();
-            } 
-            else
-            {
-                Console.WriteLine("No user input required in non-interactive mode. Starting automatic game mode.");
-                while (true)
-                {
-                    contoller.Tick();
-                    System.Threading.Thread.Sleep(10); 
-                }
-            }
-        }
-
-        static void DisplayMenu()
-        {
             // Make a menu
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("{0}{1}{2}", TOP_LEFT_JOINT, new string(HORIZONTAL_LINE, 50), TOP_RIGHT_JOINT);
@@ -93,10 +47,24 @@ namespace _54HD
             Console.Write("{0,-20}", "- For Go/Stop action press ENTER");
             Console.SetCursorPosition(5, 8);
             Console.Write("{0,-20}", "- For Exit press ESC");
-        }
 
-        static void RunInteractiveMenu()
-        {
+            // Create a time for Tick event
+            Timer timer = new Timer(10);
+            // Hook up the Elapsed event for the timer. 
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+
+            // Connect GUI with the Controller and vice versa
+            contoller = new EnhancedReactionController();
+            gui = new Gui();
+            gui.Connect(contoller);
+            contoller.Connect(gui, new RandomGenerator());
+
+            //Reset the GUI
+            gui.Init();
+            // Start the timer
+            timer.Enabled = true;
+
             // Run the menu
             bool quitePressed = false;
             while (!quitePressed)
